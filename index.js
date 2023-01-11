f3020418f2b8a6e3558135128152fa9094da36d7
nikita <nikita45454@gmail.com>
2023-01-11 19:31:47 +0300

    feat: разрашает корсы

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import routes from '#app/routes/index.js';
+import cors from 'cors';
 import { engine } from 'express-handlebars';
 
 const app = express();
+
+app.use(cors());
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
