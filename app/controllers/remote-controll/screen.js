3619737286b1e7cc4214afc72787a9e80ab1dc9a
nikita <nikita45454@gmail.com>
2023-01-24 23:19:45 +0300

    feat: адаптирует под spa

--- app/controllers/remote-controll/screen.js
+++ app/controllers/remote-controll/screen.js
@@ -2,7 +2,7 @@ import { isAuth } from './login.js';
 
 export default function (request, response) {
   if(isAuth(request.cookies['Authorization'])) {
-    response.render('remote-controll/screen');
+    response.render('remote-controll/screen', { layout: false });
   } else {
     response.redirect('/login');
   }
