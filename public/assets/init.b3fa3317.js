11f24bd04554bf30e246f12620a8b7ceb16ff1ce
nikita <nikita45454@gmail.com>
2023-02-02 19:05:09 +0300

    feat: обновляет фронт

---
+++ public/assets/init.b3fa3317.js
@@ -0,0 +1,1 @@
+import{u as a,P as t}from"./index.b1ba2a49.js";var m=({router:e})=>{e.beforeEach(async(r,i,o)=>{const s=a();t.show(),await s.fetchItems(),t.hide(),o()})};export{m as default};
--- public/assets/init.fe60828f.js
+++
@@ -1,1 +0,0 @@
-import{u as a,P as t}from"./index.542afd6c.js";var m=({router:e})=>{e.beforeEach(async(r,i,o)=>{const s=a();t.show(),await s.fetchItems(),t.hide(),o()})};export{m as default};
